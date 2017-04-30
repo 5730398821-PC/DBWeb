51f883f7617fd96b630affcc6ed01b8035bb51a6
Pitchayut Chitsinpchayakun <pitchayut@Pitchayuts-MacBook-Pro.local>
2017-05-01 01:27:20 +0700

    no message

--- routes/advisor.js
+++ routes/advisor.js
@@ -32,10 +32,10 @@ router.get('/student', function(req, res, next) {
 router.post('/student', function(req, res, next) {
 
 
-    var query1 = 'select * from (select id,fname,lname,(2017 - enrolled_year) as year, sex, curriculum, gpax, status, address, email, tel_no, birthdate, disease from student where id = "' + req.body.sid + '") A join Study S on S.Student_id = A.id;'
-    var query2 = 'select award_name, contest, institute from award where sid = "' + req.body.sid + '";'
-    var query3 = 'select absent_date, absent_type, reason, Student_id as sid from absent where Student_id = "' + req.body.sid + '";'
-    var query4 = 'select cid, course_name, term, grade, credit from (select Course_id as cid, concat(academic_year, "/", semester) as term, grade from evaluation where Student_id = "' + req.body.sid + '") A join course C on C.id = A.cid where term = "2015/1"'
+    var query1 = `select * from (select id,fname,lname,(2017 - enrolled_year) as year, sex, curriculum, gpax, status, address, email, tel_no, birthdate, disease from student where id =   ${req.body.sid}  ) A join Study S on S.Student_id = A.id;`
+    var query2 = `select award_name, contest, institute from award where sid =  ${req.body.sid} ;`
+    var query3 = `select absent_date, absent_type, reason, Student_id as sid from absent where Student_id =   ${req.body.sid} ;`
+    var query4 = `select cid, course_name, term, grade, credit from (select Course_id as cid, concat(academic_year, "/", semester) as term, grade from evaluation where Student_id =   ${req.body.sid}  ) A join course C on C.id = A.cid where term = "2015/1"`
 
     var return_data = {}
 
